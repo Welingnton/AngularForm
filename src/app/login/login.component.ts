1c8ba37dfcea059dace886e2a1dd89d13229d86f
Welingnton <wellington902008@hotmail.com>
2022-04-15 11:34:41 -0300

    UC10

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms'
+import {FormGroup, FormBuilder} from '@angular/forms'
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
  
    public loginForm!: FormGroup
-  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
+   constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -19,11 +19,12 @@ export class LoginComponent implements OnInit {
       password:['']
     })
   }
+
   login(){
-   this.http.get<any>(" http://localhost:3000/signupUsers")
+   this.http.get<any>("http://localhost:3000/signupUsers")
    .subscribe(res=>{
     const user = res.find((a:any)=>{
-      return a.email === this.loginForm.value.email && a.email === this.loginForm.value.password
+      return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
     });
     if(user){
       alert("Login Success !!");
